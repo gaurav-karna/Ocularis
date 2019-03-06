bf4a30aab1a007bdeaea329e7739dcf3bb293adb
shaaran Lakshminarayanan <34472894+devshaaran@users.noreply.github.com>
2019-03-07 01:19:43 +0530

    Gtts --> Mtts
    Upgraded

--- Ocularis_PY_Platform/main_pc_version.py
+++ Ocularis_PY_Platform/main_pc_version.py
@@ -78,14 +78,62 @@ def speak_label(mytext):
 
 
 
-def modular_speech(mytext):
-    language = 'en'
-    now = datetime.datetime.now()
-    uid = str(now.date()) + str(now.hour) + str(now.minute) + str(now.second)
-    myobj = gTTS(text=mytext, lang=language, slow=False)
-    myobj.save(os.path.join(os.getcwd(),'majortempaud' ,uid+'.mp3'))
-    # Playing the converted file
-    playsound.playsound(os.path.join(os.getcwd(),'majortempaud' ,uid+'.mp3'))
+class TextToSpeech(object):
+    def __init__(self, subscription_key,texty):
+        self.subscription_key = subscription_key
+        self.tts = texty
+        self.timestr = time.strftime("%Y%m%d-%H%M")
+        self.access_token = None
+
+
+def get_token(self):
+    fetch_token_url = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+    headers = {
+        'Ocp-Apim-Subscription-Key': self.subscription_key
+    }
+    response = requests.post(fetch_token_url, headers=headers)
+    self.access_token = str(response.text)
+
+
+def save_audio(self,uid):
+    base_url = 'https://westus.tts.speech.microsoft.com/'
+    path = 'cognitiveservices/v1'
+    constructed_url = base_url + path
+    headers = {
+        'Authorization': 'Bearer ' + self.access_token,
+        'Content-Type': 'application/ssml+xml',
+        'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+        'User-Agent': 'Ocularis_TTS'
+    }
+    xml_body = ElementTree.Element('speak', version='1.0')
+    xml_body.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-us')
+    voice = ElementTree.SubElement(xml_body, 'voice')
+    voice.set('{http://www.w3.org/XML/1998/namespace}lang', 'en-US')
+    voice.set('name', 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)')
+    voice.text = self.tts
+    body = ElementTree.tostring(xml_body)
+
+    response = requests.post(constructed_url, headers=headers, data=body)
+    if response.status_code == 200:
+        with open(os.path.join(os.getcwd(), 'majortempaud', uid + '.wav'), 'wb') as audio:
+            audio.write(response.content)
+            print("\nStatus code: " + str(response.status_code) + "\nYour TTS is ready for playback.\n")
+    else:
+        print("\nStatus code: " + str(response.status_code) + "\nSomething went wrong. Check your subscription key and headers.\n")
+
+def modular_speech(text):
+    try:
+        subscription_key = "51140fb620194c33b1e60d3df44bfd1f"
+        now = datetime.datetime.now()
+        uid = str(now.date()) + str(now.hour) + str(now.minute) + str(now.second)
+        app = TextToSpeech(subscription_key,text)
+        get_token(app)
+        save_audio(app,uid)
+        playsound.playsound(os.path.join(os.getcwd(), 'majortempaud', uid + '.wav'))
+    except Exception as e:
+        print(e)
+        save_speech('error')
+
 
 
 def naviagtor(mlon, mlat, loc):
